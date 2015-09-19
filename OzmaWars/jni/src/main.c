9db488499682c1ac53deb5befe22b6aae31c5fb0
FPierre <pflauder@gmail.com>
2015-09-19 15:20:16 +0200

    Premiers tests d'archi des Ships

--- OzmaWars/jni/src/main.c
+++ OzmaWars/jni/src/main.c
@@ -4,11 +4,40 @@
 
 #include "SDL.h"
 
-/** 
- * Test: draw a circle
- **/
-int main(int argc, char *argv[]) {
+typedef struct Weapon Weapon;
+struct Weapon {
+    int strength;
+};
 
+typedef struct {
+    int x;
+    int y;
+    int size;
+    int health;
+    int max_health;
+    Weapon weapons;
+    SDL_Rect rectangle;
+} Ship;
+
+Ship *init(int x, int y, int size, int max_health) {
+    Ship *ship = malloc(sizeof(Ship));
+    ship->x = x;
+    ship->y = y;
+    ship->size = size;
+    ship->health = ship->max_health = max_health;
+    ship->rectangle.x = ship->x;
+    ship->rectangle.y = ship->y;
+    ship->rectangle.w = ship->size;
+    ship->rectangle.h = ship->size;
+
+    return ship;
+}
+
+// void step_ship(Ship *ship);
+// void blit_ship(Ship *ship, SDL_Surface *target);
+// void kb_control(Ship *player, Uint8 *keys);
+
+int main(int argc, char *argv[]) {
     SDL_Window *window;
     SDL_Renderer *renderer;
 
@@ -18,48 +47,33 @@ int main(int argc, char *argv[]) {
     Uint8 done = 0;
     SDL_Event event;
 
+    Ship *ship;
+    int x = 0;
+
     while (!done) {
+        ship = init(x, 0, 200, 100);
+
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT 
-                    || event.type == SDL_KEYDOWN 
-                    || event.type == SDL_FINGERDOWN)
-                done = 1;
+            switch (event.type) {
+                case SDL_QUIT:
+                    done = 1;
+                    break;
+
+                case SDL_MOUSEMOTION:
+                    ship->rectangle.x += 2;
+                    break;
+            }
         }
 
         SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        int perimeter = 0;
-        int axisX = 0;
-        int axisY = 60;
-        int posX = 160;
-        int posY = 200;
-
-        SDL_RenderDrawPoint(renderer, posX + axisX, posY - axisY);
-
-        for (axisX; axisX <= axisY; axisX++) {
-            if (perimeter < 0) {
-                axisY = axisY;
-                perimeter = perimeter + (10 * axisX) + 6;
-            } else {
-                axisY -= 1;
-                perimeter = perimeter + (10 * (axisX - axisY));
-            }
-
-            SDL_RenderDrawPoint(renderer, posX + axisX, posY - axisY);
-            SDL_RenderDrawPoint(renderer, posX - axisX, posY - axisY);
-            SDL_RenderDrawPoint(renderer, posX + axisX, posY + axisY);
-            SDL_RenderDrawPoint(renderer, posX - axisX, posY + axisY);
-            SDL_RenderDrawPoint(renderer, posX + axisY, posY - axisX);
-            SDL_RenderDrawPoint(renderer, posX - axisY, posY - axisX);
-            SDL_RenderDrawPoint(renderer, posX + axisY, posY + axisX);
-            SDL_RenderDrawPoint(renderer, posX - axisY, posY + axisX);
-        }
+        SDL_RenderFillRect(renderer, &(ship->rectangle));
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
         SDL_RenderPresent(renderer);
-        SDL_Delay(5000);
+        SDL_Delay(10);
     }
-    
+
     exit(0);
 }
